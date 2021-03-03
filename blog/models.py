f2780fa84197b19efce36ab19659eafa2583f69f
elMeniwy <55510819+elMeniwy@users.noreply.github.com>
2021-03-03 08:49:28 +0200

    Comments - Views
    Added Comments And views

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    post_date = models.DateTimeField(default=timezone.now)
+    post_update = models.DateField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.author}  {self.title}'
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    body = models.TextField()
+    comment_date = models.DateField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+    # realted_name -> used when we want to call the comments from the posts in the view
+
+    def __str__(self):
+        return f'''
+علق
+{self.name}
+علي التدوينة
+({self.post.title})
+                '''
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post,Comment
+
+
 
 
 def home(request):
-    return render(request, 'blog/index.html')
+    context = {
+        'title': 'الصفحة الرئيسية',
+        'posts': Post.objects.all().order_by('-post_date'),
+    }
+    return render(request, 'blog/index.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'من انا'
+    }
+    return render(request, 'blog/about.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'title': 'title_for_now',
+        'post': post,
+        'comments': post.comments.filter(active=True).order_by('-comment_date')
+    }
+    return render(request, 'blog/detail.html', context)
+
